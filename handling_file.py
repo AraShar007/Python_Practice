d6dcbc27e560e6e5a27b996fd17f32007912388d
Aravi <aravisharma07@gmail.com>
2024-07-18 16:19:23 +0530

    loops basic

--- handling_file.py
+++ handling_file.py
@@ -1,16 +1,17 @@
 # to count no of lines in a text file
 # use readlines()
-# with open("Text_File.txt", "r")as file:
-#     # print(file.readlines())
-#     num_of_line = len(file.readlines())
-#     print("No of lines:", num_of_line)
+with open("Text_File.txt", "r")as file:
+    # print(file.readlines())
+    num_of_line = len(file.readlines())
+    file.seek(0)
+    print("No of lines:", num_of_line)
 #
 #using for loop
-with open("Text_File.txt", "rt") as file:
-    num_of_line=0
-    for line in file:
-        num_of_line += 1
-    print("No of lines:", num_of_line)
+# with open("Text_File.txt", "rt") as file:
+#     num_of_line=0
+#     for line in file:
+#         num_of_line += 1
+#     print("No of lines:", num_of_line)
 
 
 
